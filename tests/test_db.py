47a3aa66513f815111e6ae863d764d2b58b25dbe
Timothee Chauvin <timothee.chauvin28@gmail.com>
2024-05-25 17:59:45 +0200

    add test to check that there are no stale revisions

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,20 +1,36 @@
-from pathlib import Path
-
+import pytest
 from sqlmodel import Session, create_engine, select
 
 from eyeballvul.api import json_import
 from eyeballvul.models.eyeballvul import EyeballvulItem, EyeballvulRevision
 
 
-def test_db_integrity(tmpdir):
-    """Check that for each commit in each EyeballvulItem, the corresponding revision exists and has
-    the same repo URL."""
-    db_path = Path(tmpdir) / "db"
+@pytest.fixture(scope="module")
+def db_session(tmp_path_factory):
+    tmpdir = tmp_path_factory.mktemp("data")
+    db_path = tmpdir / "db"
     json_import(db_path)
     engine = create_engine(f"sqlite:///{db_path}/eyeballvul.db")
     with Session(engine) as session:
-        for item in session.exec(select(EyeballvulItem)).all():
-            for commit in item.commits:
-                revision = session.get(EyeballvulRevision, commit)
-                assert revision is not None
-                assert revision.repo_url == item.repo_url
+        yield session
+
+
+def test_db_commit_to_revision_integrity(db_session):
+    """Check that for each commit in each EyeballvulItem, the corresponding revision exists and has
+    the same repo URL."""
+    for item in db_session.exec(select(EyeballvulItem)).all():
+        for commit in item.commits:
+            revision = db_session.get(EyeballvulRevision, commit)
+            assert revision is not None
+            assert revision.repo_url == item.repo_url
+
+
+def test_db_stale_revisions(db_session):
+    """Check that there are no revisions without a corresponding EyeballvulItem."""
+    revision_commits = {
+        revision.commit for revision in db_session.exec(select(EyeballvulRevision)).all()
+    }
+    item_commits = {
+        commit for item in db_session.exec(select(EyeballvulItem)).all() for commit in item.commits
+    }
+    assert revision_commits == item_commits
