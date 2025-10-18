e36f62b64d198a1d03274a5c7ed09aa7d2ed06bf
Timothee Chauvin <timothee.chauvin28@gmail.com>
2025-10-18 14:36:56 +0200

    bugfix: support CVSS v4

--- eyeballvul/models/common.py
+++ eyeballvul/models/common.py
@@ -4,5 +4,5 @@ from typing_extensions import TypedDict
 
 
 class Severity(TypedDict):
-    type: Literal["CVSS_V3", "CVSS_V2", "UNSPECIFIED"]
+    type: Literal["CVSS_V2", "CVSS_V3", "CVSS_V4", "UNSPECIFIED"]
     score: str
